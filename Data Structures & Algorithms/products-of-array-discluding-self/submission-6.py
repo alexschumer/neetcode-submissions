f2ad9ce1eacfc63a421dfd5507bd571f40a23ead
Alex Schumer <alexander.schumer@gmail.com>
2026-09-23 11:12:57 -0700

    Add: products-of-array-discluding-self - submission-6

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-6.py
@@ -0,0 +1,17 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        # it does not seem like we are concerned with duplicates.
+        # we do need to think about looping to the begining of the array from the end.
+        # problem: multiply all the elements in the nums list not including selft (ith) index.
+        # i dont think hashmap or set will help us here. we can use two pointers to navigate the window easily.
+        left = [1] * len(nums)
+        right = [1] * len(nums)
+        res = [1] * len(nums)
+
+        for i in range(1,len(nums)):
+            left[i] = left[i - 1] * nums[i - 1]
+        for i in range(len(nums)-2, -1, -1):
+            right[i] = right[i+1] * nums[i+1]
+        for i in range(len(nums)):
+            res[i] = left[i] * right[i]
+        return res
